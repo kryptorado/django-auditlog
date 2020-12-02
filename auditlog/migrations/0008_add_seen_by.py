6f4f4981499b726befcb73c31dfdcb22eb550994
Dora Goczi <dbgoczi@edu.uwaterloo.ca>
2020-12-02 13:31:11 -0500

    add new field

---
+++ auditlog/migrations/0008_add_seen_by.py
@@ -0,0 +1,22 @@
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auditlog', '0007_object_pk_type'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='logentry',
+            name='seen_by',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.SET_NULL,
+                blank=True,
+                to=settings.AUTH_USER_MODEL,
+                null=True
+            ),
+        ),
+    ]
